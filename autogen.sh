f10be6abd4f0b0dad8498d6896135d2994f7ca8e
Joachim Bauch <bauch@struktur.de>
2014-05-07 09:33:18 +0200

    Get GStreamer "common" module and check indent on commit.

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
+
+# Make sure we have common
+if test ! -f common/gst-autogen.sh;
+then
+  echo "+ Setting up common submodule"
+  git submodule init
+fi
+git submodule update
+
+# install pre-commit hook for doing clean commits
+if test ! \( -x .git/hooks/pre-commit -a -L .git/hooks/pre-commit \);
+then
+    rm -f .git/hooks/pre-commit
+    ln -s ../../common/hooks/pre-commit.hook .git/hooks/pre-commit
+fi
+
 if [ -x "`which autoreconf 2>/dev/null`" ] ; then
    exec autoreconf -ivf
 fi
